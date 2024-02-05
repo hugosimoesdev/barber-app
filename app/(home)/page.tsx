eb2ef5c61cc2da60ef916dbee2b1d44e5f1649ca
Hugo <hugosimoescontato@gmail.com>
2024-02-05 16:20:11 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,20 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div className="w-full">
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Hugo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
+  );
 }
